d643840ebb0b7d38b033f52edeb19ab4d7d354e2
Physic_Hybrid <85246569+PhysicHybridTG@users.noreply.github.com>
2021-06-06 20:04:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ import os
 import zipfile
 import pathlib
 
-bot_token = os.environ.get("1847404351:AAHh077QxeHHtGq6uN5k2TGoJ19NcRmxHm4", "")
+bot_token = os.environ.get("BOT_TOKEN", "")
 bot = Bot(token=bot_token)
 
 help_keyboard = [[InlineKeyboardButton("Updates Channel", url="https://t.me/MBNUpdates"),
